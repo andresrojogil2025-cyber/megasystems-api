55979407dd27874c01f2202878ceefb666253d91
Megasystems <megasystems@correo.com>
2026-09-14 21:58:16 -0400

    Fix: quitar --single-process de Chromium (causaba crash)

--- backend/controllers/whatsappController.js
+++ backend/controllers/whatsappController.js
@@ -24,7 +24,6 @@ const initializeWPClient = () => {
                 '--disable-accelerated-2d-canvas',
                 '--no-first-run',
                 '--no-zygote',
-                '--single-process',
                 '--disable-gpu'
             ],
         }
